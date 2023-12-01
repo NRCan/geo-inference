41ada02ca4fb526fcaa7e89ef6e001658b3cf691
victor alhassan <victorlazio109@yahoo.com>
2023-11-30 19:57:55 -0500

    Refactor test_helpers.py

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -14,14 +14,17 @@ from geo_inference.utils.helpers import (calculate_gpu_stats,
                                          is_tiff_path, is_tiff_url, read_yaml,
                                          validate_asset_type)
 
+@pytest.fixture
+def tmp_path():
+    return Path(__file__).parent.parent / "data"
 
 @pytest.fixture
-def temp_tar_gz_file():
-    tmp_path = Path(__file__).parent.parent / "data"
+def temp_tar_gz_file(tmp_path):
     tar_gz_content = b'test_content'
     tar_gz_file_path = tmp_path / 'test.tar.gz'
+    tif = str(tmp_path / 'test.tiff')
     with tarfile.open(tar_gz_file_path, 'w:gz') as tar:
-        tarinfo = tarfile.TarInfo('test.tiff')
+        tarinfo = tarfile.TarInfo(tif)
         tarinfo.size = len(tar_gz_content)
         tar.addfile(tarinfo, fileobj=io.BytesIO(tar_gz_content))
     return tar_gz_file_path
@@ -43,23 +46,24 @@ def test_read_yaml():
         assert result == {"key": "value"}
         mock_open.assert_called_once_with('test.yaml', 'r')
 
-def test_validate_asset_type():
+def test_validate_asset_type(tmp_path):
     with patch('geo_inference.utils.helpers.is_tiff_path', return_value=True), patch('geo_inference.utils.helpers.is_tiff_url', return_value=True):
-        assert validate_asset_type('test.tiff') == 'test.tiff'
+        tiff = str(tmp_path / 'test.tiff')
+        assert validate_asset_type(tiff) == tiff
         assert validate_asset_type('http://example.com/test.tiff') == 'http://example.com/test.tiff'
 
 def test_calculate_gpu_stats():
     with patch('torch.cuda.utilization', return_value=50), patch('torch.cuda.mem_get_info', return_value=(500, 1000)):
         assert calculate_gpu_stats() == ({'gpu': 50}, {'used': 500, 'total': 1000})
 
-def test_download_file_from_url():
+def test_download_file_from_url(tmp_path):
     with patch('requests.get') as mocked_get:
         mocked_get.return_value.status_code = 200
         mocked_get.return_value.iter_content.return_value = [b'test']
-        download_file_from_url('http://example.com/test.tiff', 'test.tiff')
+        tif = str(tmp_path / 'test.tiff')
+        download_file_from_url('http://example.com/test.tiff', tif)
 
-def test_extract_tar_gz(temp_tar_gz_file):
-    tmp_path = Path(__file__).parent.parent / "data"
+def test_extract_tar_gz(temp_tar_gz_file, tmp_path):
     target_directory = tmp_path
     extract_tar_gz(temp_tar_gz_file, target_directory)
     extracted_file_path = tmp_path / 'test.tiff'
@@ -82,7 +86,7 @@ def test_get_model_with_invalid_model():
         with pytest.raises(ValueError, match="Invalid model name"):
             get_model("invalid_model", Path("work_dir"))
 
-def test_get_model_with_missing_geosys_token():
+def test_get_model_with_missing_geosys_token(tmp_path):
     with patch('geo_inference.utils.helpers.read_yaml') as mock_read_yaml, \
          patch('geo_inference.utils.helpers.Path') as mock_path:
         mock_read_yaml.return_value = {"model1": {"url": "https://example.com/model1.tar.gz"}}
@@ -91,4 +95,6 @@ def test_get_model_with_missing_geosys_token():
         # Mocking os.environ to simulate missing GEOSYS_TOKEN
         with patch.dict(os.environ, clear=True):
             with pytest.raises(KeyError):
-                get_model("model1", Path("work_dir"))
+                get_model("model1", tmp_path)
+        
+            
