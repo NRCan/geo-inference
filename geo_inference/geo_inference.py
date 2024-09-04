56e464307a8cceb43c45b6d22ac62aa031e6b80a
Turgeon-Pelchat <mathieu.turgeon-pelchat@nrcan-rncan.gc.ca>
2024-09-04 08:13:05 -0400

    update function docstring

--- geo_inference/geo_inference.py
+++ geo_inference/geo_inference.py
@@ -55,6 +55,7 @@ class GeoInference:
         multi_gpu (bool): Whether to run the inference on multi-gpu or not.
         gpu_id (int): The ID of the GPU to use for inference (if device is "gpu").
         num_classes (int) : The number of classes in the output of the model.
+        prediction_threshold (float): Prediction probability Threshold (fraction of 1) to use.
 
     Attributes:
         work_dir (Path): The directory where the model and output files will be saved.
@@ -145,6 +146,7 @@ class GeoInference:
             inference_input Union[Path, str]: The path/url to the geospatial image to perform inference on.
             bands_requested List[str]: The requested bands to consider for the inference.
             patch_size (int): The size of the patches to use for inference.
+            workers (int): Number of workers used by dask, Default = Nb of cores available on the host, minus 1.
             bbox (str): The bbox or extent of the image in this format "minx, miny, maxx, maxy".
 
         Returns:
